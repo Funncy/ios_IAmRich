421cbb90b1a77972aa5a988b228df4c6c3508b47
김효준 <gywns41@todo-works.com>
2019-12-05 10:08:47 +0900

    Initial Commit

---
+++ I AM Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by 김효준 on 05/12/2019.
+//  Copyright © 2019 Hyojun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
